b8cd0ac65ccec1457de98289d4d26ce23a5d7abf
CaptainJAV <nvatn0706@gmail.com>
2021-03-25 17:15:20 +0700

    first commit

--- Graduate-Essay/drowsiness.py
+++ Graduate-Essay/drowsiness.py
@@ -1,131 +1,141 @@
-from scipy.spatial import distance as dist 
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 from imutils import face_utils
-from threading import Thread
-import numpy as np 
-import playsound
+import numpy as np
 import argparse
 import imutils
 import time
-import dlib 
-import cv2 
-import RPi.GPIO as GPIO
+import dlib
+import cv2
 
-#play sound
-def sound_alarm(path):
-    playsound.playsound(path)
-
-#compute the ratio of distances between the vertial eye landmarks and the distances between the horizonal eye landmarks
 def eye_aspect_ratio(eye):
-    #vertical eye landmarks (x,y)-coordinates
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
-    C = dist.euclidean(eye[0], eye[3])
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
 
-    #compute the eye aspect ratio
-    ear = (A + B) / (2.0 * C)
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
 
-    return ear
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
 
-#parse the commandline
+	# return the eye aspect ratio
+	return ear
+
+# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--cascade", required=True, help= "path to where the face cascade resides") #--cascade is the path to the Haar cascade XML file used for face detection
-ap.add_argument("-p", "--shape-predictor", required=True, help= "path to facial landmark predictor") #--shape-predictor is the path to the dlib facial landmark predictor file
-ap.add_argument("-a", "--alarm", type=int, default=0, help= "boolean used to indicate if TrafficHat should be used") #--alarm is a boolean to indicate if the TrafficHat buzzer should be used when drowsiness is detected
-ap.add_argument("-w", "--webcam", type=int, default=0, help= "index of webcam on system")
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-v", "--video", type=str, default="",
+	help="path to input video file")
 args = vars(ap.parse_args())
 
-#check to see if we are using GPIO/TrafficHat as an alarm
-if args["alarm"] > 0:
-    from gpiozero import TrafficHat
-    th = TrafficHat()
-    print("[INFO] using TrafficHat alarm ...")
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold
+EYE_AR_THRESH = 0.23
+EYE_AR_CONSEC_FRAMES = 3
 
-#define variables
-EYE_AR_THRESH = 0.3 #If the eye aspect ratio falls below this threshold, we’ll start counting the number of frames the person has closed their eyes for.
-EYE_AR_CONSEC_FRAMES = 48 #If the number of frames the person has closed their eyes in exceeds, we'll sound an alarm
+# initialize the frame counters and the total number of blinks
+COUNTER = 0
+TOTAL = 0
 
-COUNTER = 0 #the total number of consecutive frames where the eye aspect ratio is below EYE_AR_THRESH
-ALARM_ON = False
-
-#initialize dlib's face detector (HOG-based) and then create the facial landmark predictor
-print("[INFO] loading facial landmark predictor ....")
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 
-#extract the eye regions from a set of facial landmarks
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-#core of drowsiness detector
-print("[INFO] starting video stream thread ...")
-vs = VideoStream(src=args["webcam"]).start()
+# start the video stream thread
+print("[INFO] starting video stream thread...")
+# vs = FileVideoStream(args["video"]).start()
+# fileStream = True
+vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+fileStream = False
 time.sleep(1.0)
 
-#loop over frames from the video stream
+# loop over frames from the video stream
 while True:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=450)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# if this is a file video stream, then we need to check if
+	# there any more frames left in the buffer to process
+	if fileStream and not vs.more():
+		break
 
-    #detect faces in the grayscale frame
-    rects = detector(gray, 0)
+	# grab the frame from the threaded video file stream, resize
+	# it, and convert it to grayscale
+	# channels)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #loop over the face detections
-    for rect in rects:
-        #determine the facial landmarks for the face region, then convert the facial landmark (x, y)-coordinates to a NumPy array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+	# detect faces in the grayscale frame
+	rects = detector(gray, 0)
 
-        #extract left and right eye coordinates, then use the coordinate to compute the eye aspect ratio for both eyes
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
+	# loop over the face detections
+	for rect in rects:
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
 
-        #average the eye aspect ratio together for both eyes
-        ear = (leftEAR + rightEAR) / 2.0
+		# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
 
-        #compute the convex hull for the left and right eye, then visualize each of the eyes
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frames, [leftEyeHull], -1, (0,255,0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0,255,0), 1)
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
 
-        #check to see if the eye aspect ratio is below the blink threshold, and if so, increment the blink frame counter
-        if ear < EYE_AR_THRESH:
-            COUNTER += 1 
+		# compute the convex hull for the left and right eye, then
+		# visualize each of the eyes
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-            #if the eye were closed for a sufficient number of then sound the alarm
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                if not ALARM_ON:
-                    ALARM_ON = True
+		# check to see if the eye aspect ratio is below the blink
+		# threshold, and if so, increment the blink frame counter
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
 
-                    if args["alarm"] != "":
-                        t = Thread(target=sound_alarm, args=(args["alarm"],))
-                        t.daemon = True
-                        t.start()
+		# otherwise, the eye aspect ratio is not below the blink
+		# threshold
+		else:
+			# if the eyes were closed for a sufficient number of
+			# then increment the total number of blinks
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				TOTAL += 1
 
-                #draw an alarm on the frame
-                cv2.putText(frame, "Drowsiness Alert!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+			# reset the eye frame counter
+			COUNTER = 0
 
-        else:
-            COUNTER = 0
-            ALARM_ON = False
-            cv2.putText(frame, "EAR: {:2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		# draw the total number of blinks on the frame along with
+		# the computed eye aspect ratio for the frame
+		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-    #show the frame 
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+	# show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
 
-    #if the 'q' key was pressed, break from the loop
-    if key == ord('q'):
-        break
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
+# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
-
-''' Running command line to execute this file: 
-$ python detect_drowsiness.py \
-	--shape-predictor shape_predictor_68_face_landmarks.dat \
-	--alarm alarm.wav '''
